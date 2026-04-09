e36cd78a1e792001de504b13e1d2e3faeadc0dd2
Christina Kuo <kuochr58@students.rowan.edu>
2026-04-09 19:48:51 -0400

    voice

--- database/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Doctor, Test, Stimulus, Response, Results, Latency
-
-admin.site.register(Doctor)
-admin.site.register(Test)
-admin.site.register(Stimulus)
-admin.site.register(Response)
-admin.site.register(Results)
-admin.site.register(Latency)
